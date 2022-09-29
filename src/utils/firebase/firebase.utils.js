10a0f821fdfebb9732f40055e50ec6db235577eb
AndriiChemerys <andriichemerys@gmail.com>
2022-09-29 20:59:54 +0200

    92. Authenticating With Firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdDcZUJ1FWDC6r_E4_MLSn2h3K_icKdEA",
+  authDomain: "crwn-clothing-db-6158e.firebaseapp.com",
+  projectId: "crwn-clothing-db-6158e",
+  storageBucket: "crwn-clothing-db-6158e.appspot.com",
+  messagingSenderId: "632109806540",
+  appId: "1:632109806540:web:b8e470dcb01cadea9ce369",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
